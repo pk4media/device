8b90d035ac14265f17e197a41a0d4a6b07e330be
David Negstad <davidn@pk4media.com>
2017-01-17 17:00:45 -0800

    Detect evaliant bot trying to disguise itself

--- test/device_bot_desktop_test.js
+++ test/device_bot_desktop_test.js
@@ -2445,15 +2445,21 @@ describe('device', function() {
             assert.equal(mydevice.is('bot'), true);
         });
     });
-    describe('AdBeat Bot', function() {
+    describe('AdBeat Bot', function () {
         it('should get true', function() {
             var mydevice = device('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:44.0) adbeat.com/policy Gecko/20100101 Firefox/44.0');
             assert.equal(mydevice.is('bot'), true);
         });
     });
-    describe('Kantar Media Evaliant Bot', function() {
+    describe('Kantar Media Evaliant Bot', function () {
         it('should get true', function() {
             var mydevice = device('Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36 evaliant');
+            assert.equal(mydevice.is('bot'), true);
+        });
+    });
+    describe('Kantar Media Disguised Bot', function () {
+        it('should get true', function() {
+            var mydevice = device('Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36');
             assert.equal(mydevice.is('bot'), true);
         });
     });
